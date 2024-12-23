eb59dac03795cf5a2494b38a11fd3f01a56a791e
Ramesh Paswan <r6360986@gmail.com>
2024-12-24 00:38:09 +0530

    update method
           /*
    		 *insert Records
    		 * Student entity = new Student(); entity.setId(1);
    		 * entity.setName("Ramesh Paswan"); entity.setGender("Male");
    		 * entity.setRank(59); studentRepository.save(entity);
    		 * System.out.println("Record Inserted.....");
    		 */
    		
    		//Retrive Records single entity.
    		
    		/*
    		 * Optional<Student> id = studentRepository.findById(1);
    		 * 
    		 * if(id.isPresent()) { Student student = id.get(); List<Student> list =
    		 * List.of(student); for(Student s : list) { System.out.println(s); 
    		 * } }
    		 */
    		
    		// Retrive Records All Entity
    		/*
    		 * Iterable<Student> all = studentRepository.findAll();
    		 * studentRepository.findAll().forEach(a->System.out.println(a));
    		 * System.out.println("OR===========");
    		 * all.forEach(entity->System.out.println(entity));
    		 */
    		
    		//delete Recode by id;
    		/*
    		 * if(studentRepository.existsById(10)) { studentRepository.deleteById(10);
    		 * System.out.println("Record Deleted..."); }else {
    		 * System.out.println("No Records Present.."); }
    		 */
    		//how many Record available 
    		long count = studentRepository.count();
    		System.out.println(count);

--- Application.java
+++ Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import in.Ramesh.entity.Student;
 import in.Ramesh.repository.StudentRepository;
 
 @SpringBootApplication
@@ -14,15 +13,42 @@ public class Application {
 		ConfigurableApplicationContext context = 
 				SpringApplication.run(Application.class, args);
 		StudentRepository studentRepository = context.getBean(StudentRepository.class);
-	//	System.out.println(studentRepository.getClass().getName());
+		System.out.println(studentRepository.getClass().getName());
+		/*
+		 *insert Records
+		 * Student entity = new Student(); entity.setId(1);
+		 * entity.setName("Ramesh Paswan"); entity.setGender("Male");
+		 * entity.setRank(59); studentRepository.save(entity);
+		 * System.out.println("Record Inserted.....");
+		 */
 		
-		Student entity = new Student();
-		entity.setId(1);
-		entity.setName("Ramesh Paswan");
-		entity.setGender("Male");
-		entity.setRank(59);
-		studentRepository.save(entity);
-		System.out.println("Record Inserted.....");
+		//Retrive Records single entity.
+		
+		/*
+		 * Optional<Student> id = studentRepository.findById(1);
+		 * 
+		 * if(id.isPresent()) { Student student = id.get(); List<Student> list =
+		 * List.of(student); for(Student s : list) { System.out.println(s); 
+		 * } }
+		 */
+		
+		// Retrive Records All Entity
+		/*
+		 * Iterable<Student> all = studentRepository.findAll();
+		 * studentRepository.findAll().forEach(a->System.out.println(a));
+		 * System.out.println("OR===========");
+		 * all.forEach(entity->System.out.println(entity));
+		 */
+		
+		//delete Recode by id;
+		/*
+		 * if(studentRepository.existsById(10)) { studentRepository.deleteById(10);
+		 * System.out.println("Record Deleted..."); }else {
+		 * System.out.println("No Records Present.."); }
+		 */
+		//how many Record available 
+		long count = studentRepository.count();
+		System.out.println(count);
 	}
 
 }
